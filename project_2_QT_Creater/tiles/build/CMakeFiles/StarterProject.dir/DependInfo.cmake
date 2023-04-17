17775ea3995d4c90c9a612038f5c9b4130f1a228
Minsu-Jung1999 <96643903+Minsu-Jung1999@users.noreply.github.com>
2023-04-16 23:03:26 -0700

    04-16
    직사각형 그리는 법은 알았는데 다음은 어캐하냐...

---
+++ project_2_QT_Creater/tiles/build/CMakeFiles/StarterProject.dir/DependInfo.cmake
@@ -0,0 +1,94 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/StarterProject_autogen/mocs_compilation.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/StarterProject_autogen/mocs_compilation.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/console.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/console.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gbrowserpane.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gbrowserpane.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gbutton.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gbutton.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gcanvas.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gcanvas.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gcheckbox.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gcheckbox.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gchooser.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gchooser.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gclipboard.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gclipboard.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gcolor.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gcolor.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gcolorchooser.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gcolorchooser.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gconsolewindow.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gconsolewindow.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gcontainer.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gcontainer.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gdiffgui.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gdiffgui.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gdiffimage.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gdiffimage.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gdownloader.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gdownloader.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gdrawingsurface.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gdrawingsurface.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gevent.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gevent.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/geventqueue.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/geventqueue.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gexceptions.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gexceptions.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gfilechooser.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gfilechooser.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gfont.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gfont.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gfontchooser.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gfontchooser.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/ginit.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/ginit.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/ginteractor.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/ginteractor.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/glabel.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/glabel.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/glayout.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/glayout.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gmath.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gmath.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gobjects.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gobjects.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gobservable.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gobservable.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/goptionpane.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/goptionpane.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gradiobutton.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gradiobutton.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gscrollbar.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gscrollbar.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gscrollpane.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gscrollpane.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gslider.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gslider.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gsound.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gsound.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gspacer.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gspacer.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gtable.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gtable.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gtextarea.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gtextarea.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gtextfield.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gtextfield.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gthread.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gthread.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gtimer.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gtimer.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gtypes.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gtypes.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gversion.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gversion.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/gwindow.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/gwindow.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/librarymain.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/librarymain.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/mainwrapper.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/mainwrapper.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/privatediff.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/privatediff.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/privatefilelib.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/privatefilelib.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/privateregexpr.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/privateregexpr.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/privatestrlib.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/privatestrlib.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/qtgui.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/qtgui.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/lib/require.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/lib/require.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/src/Tile.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/src/Tile.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/src/TileMain.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/src/TileMain.cpp.obj"
+  "C:/Users/allison/Documents/132/winter22/hw2/tiles/src/TileManager.cpp" "C:/Users/allison/Documents/132/winter22/hw2/tiles/build/CMakeFiles/StarterProject.dir/src/TileManager.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_MULTIMEDIA_LIB"
+  "QT_NETWORK_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "StarterProject_autogen/include"
+  "../lib"
+  "../src"
+  "C:/Qt3/5.15.2/mingw81_64/include"
+  "C:/Qt3/5.15.2/mingw81_64/include/QtWidgets"
+  "C:/Qt3/5.15.2/mingw81_64/include/QtGui"
+  "C:/Qt3/5.15.2/mingw81_64/include/QtANGLE"
+  "C:/Qt3/5.15.2/mingw81_64/include/QtCore"
+  "C:/Qt3/5.15.2/mingw81_64/./mkspecs/win32-g++"
+  "C:/Qt3/5.15.2/mingw81_64/include/QtMultimedia"
+  "C:/Qt3/5.15.2/mingw81_64/include/QtNetwork"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
