12590ff01ee145629154f95eeb7920525f7e19f0
Minsu-Jung1999 <96643903+Minsu-Jung1999@users.noreply.github.com>
2023-05-11 22:15:59 -0700

    05-11

--- project_4/Project_4/AnagramSolver.cpp
+++ project_4/Project_4/AnagramSolver.cpp
@@ -1,4 +1,12 @@
-// replace this comment with your AnagramSolver cpp code
+/*
+* Minsu Jung
+* CS 132 Project 2
+* April 29
+*
+* This cpp file defines the Anagrams class with a constructor that takes a vector called dictionary as a parameter. 
+* It also includes the LetterInventory and AnagramSolver headers, as well as the <vector> and <iostream> headers. 
+* This cpp file provides functions to get words from a given phrase and print the results based on certain conditions.
+*/
 
 #include "AnagramSolver.h"
 #include "LetterInventory.h"
@@ -7,13 +15,13 @@
 
 using namespace std;
 
-Anagrams::Anagrams(vector<string>& dictionary):dictionary(dictionary)
-{
-}
+// Constructor for the Anagrams class that initializes the dictionary member variable with the provided vector.
+Anagrams::Anagrams(vector<string>& dictionary):dictionary(dictionary){}
 
+// Retrieves words from the given phrase by comparing it with the words in the dictionary and returns a vector of matching words.
 vector<string> Anagrams::getWords(string& phrase)
 {
-	if (!(this->wordsInPhrase.empty()))
+	if (!(this->wordsInPhrase.empty()))	// it will activate after a user input a second anagram.
 	{
 		wordsInPhrase.clear();
 	}
@@ -29,14 +37,20 @@ vector<string> Anagrams::getWords(string& phrase)
 	return wordsInPhrase;
 }
 
-void Anagrams::print(string& phrase)
+// Prints all possible anagrams of the phrase by recursively calling the printHelper function with appropriate parameters.
+const void Anagrams::print(string& phrase)
 {
 	LetterInventory letter(phrase);
 	vector <string> outputresults;
-	printHelper(letter, 0, outputresults,0);
+	int index=0;
+	int maxLevel=0;
+	printHelper(letter, index, outputresults,maxLevel);
 }
 
-void Anagrams::print(string& phrase, int max)
+// Prints anagrams of the phrase up to a maximum length of max, or all anagrams if max is 0. 
+// Uses the printHelper function to perform the recursive printing.
+// phrase is string from a user's input, max is amount of anagrams.
+const void Anagrams::print(string& phrase, const int max)
 {
 	if (phrase.size() == 0) 
 		return;
@@ -46,14 +60,36 @@ void Anagrams::print(string& phrase, int max)
 	{
 		LetterInventory letter(phrase);
 		vector <string> outputresults;
-		printHelper(letter, 0, outputresults, max);
+		int index=0;
+		printHelper(letter, index, outputresults, max);
 	}
-	
 }
 
-void Anagrams::printHelper(LetterInventory& letters,int index, vector <string> results,int maxval)
+// Helper function to print words in the given results vector with comma-separated formatting.
+const void Anagrams::printWords(const vector<string>& results)
 {
-	
+	if (results.empty())
+	{
+		return;
+	}
+	cout << results.front();
+	if (results.size() > 1)
+	{
+		cout << ", ";
+	}
+	printWords(std::vector<string>(results.begin() + 1, results.end()));
+}
+
+// Recursive helper function that generates and prints anagrams based on the provided letters (remaining letters), current index, and previous results.
+// It considers the maximum length of anagrams as maxval.
+// maxval reffers amount of anagrams for printing.
+const void Anagrams::printHelper(LetterInventory& letters,int index, vector <string> results, const int maxval)
+{
+	if (results.size() > maxval)
+	{
+		return;
+	}
+	// completed case
 	if (letters.isEmpty())
 	{
 		if (maxval != 0 && results.size()!= maxval)
@@ -63,43 +99,27 @@ void Anagrams::printHelper(LetterInventory& letters,int index, vector <string> r
 		else
 		{
 			cout << "[";
-			for (size_t i = 0; i < results.size(); i++)
-			{
-				cout << results[i];
-				if (i + 1 < results.size())
-					cout << ", ";
-			}
-
+			printWords(results);
 			cout << "]" << endl;
 			return;
-
 		}
 	}
-	if (index + 1 > wordsInPhrase.size())
+	else if (index + 1 > wordsInPhrase.size())	// to avoid out of range
 	{
 		return;
 	}
-	if (letters.contains(wordsInPhrase[index])) // abash meet bar
+	else if (letters.contains(wordsInPhrase[index])) // anagram case
 	{
 		results.push_back(wordsInPhrase[index]);
-		LetterInventory chosen(wordsInPhrase[index]);
-		LetterInventory leftletters = letters - chosen;
+		LetterInventory chosen(wordsInPhrase[index]);	// chosen word's alphabet
+		LetterInventory leftletters = letters - chosen;	// left word's alphabet
 		printHelper(leftletters, 0, results,maxval);
 		results.pop_back();
-		printHelper(letters, index + 1, results,maxval);
-		//cout << " Fisnish! " << endl;
-		//cout << " current words in phrase " << wordsInPhrase[index] << endl;
-		//cout << " current letters : " << letters << endl;
-		//cout << " current index : " << index << endl;
+		printHelper(letters, index + 1, results,maxval);	// Back Tracking (Finding all possible anagram matching)
 	}
 	else
 	{
 		printHelper(letters, index + 1, results,maxval);
 	}
-
 }
 
-bool Anagrams::containVowels(LetterInventory& letters)
-{
-	return (letters.contains("a") || letters.contains("e") || letters.contains("i") || letters.contains("o") || letters.contains("u"));
-}
--- project_4/Project_4/AnagramSolver.h
+++ project_4/Project_4/AnagramSolver.h
@@ -1,24 +1,49 @@
+/*
+* Minsu Jung
+* CS 132 Project 2
+* April 29
+*
+* The code defines the Anagrams class with a constructor that takes a dictionary, 
+* and provides functions to get words from a phrase, print anagrams, and check if a phrase contains vowels. 
+* It includes the necessary headers and uses the LetterInventory class.
+* 
+*/
 #ifndef ANAGRAMSOLVER_H
 #define ANAGRAMSOLVER_H
 
 #include <iostream>
 #include <vector>
 #include "LetterInventory.h"
+
+
 class Anagrams
 {
 private:
-	vector<string> dictionary;
-	vector<string> wordsInPhrase;
-	vector<string> anagrams;
-public:
-	Anagrams(vector<string>& dictionary);
-	vector<string> getWords(string& phrase);
-	void print(string& phrase);
-	void print(string& phrase, int max);
+	/** contains Anagrams from a file. */
+	const vector<string> dictionary;	
 	
-	// additional funcitons
-	void printHelper(LetterInventory& letters, int index, vector <string> results, int maxval);
-	bool containVowels(LetterInventory& phrase);
+	/** contains all anagrams from user's input */
+	vector<string> wordsInPhrase;
+	
+public:
+	/** Constructor that initializes the Anagrams object with the provided dictionary vector. */
+	Anagrams(vector<string>& dictionary);
+	
+	/** Returns a vector of words from the dictionary that can be formed using the letters in the given phrase. */
+	vector<string> getWords(string& phrase);
+
+	/** Prints all the anagrams of the phrase using the words from the dictionary. */
+	const void print(string& phrase);
+
+	/**Prints anagrams of the phrase up to a maximum length of max, or all anagrams if max is 0. */
+	const void print(string& phrase, const int max);
+	
+	/** Helper function that recursively generates and prints anagrams based on the remaining letters, 
+	/**	current index, previous results, and the maximum length of anagrams specified by maxval. */
+	const void printHelper(LetterInventory& letters, int index, vector <string> results, const int maxval);
+
+	/** Function added for printing recursively. */
+	const void printWords(const vector<string>& results);
 };
 
 #endif // HEADER_FILE_NAME_H
